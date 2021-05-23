d8c7880f1b840c51590508e6f18f2aef9f017782
Qiusheng Wu <giswqs@gmail.com>
2021-05-22 22:06:22 -0400

    Added folium module

--- leafmap/__init__.py
+++ leafmap/__init__.py
@@ -1,7 +1,31 @@
 """Top-level package for leafmap."""
 
 __author__ = """Qiusheng Wu"""
-__email__ = 'giswqs@gmail.com'
-__version__ = '0.0.1'
+__email__ = "giswqs@gmail.com"
+__version__ = "0.0.1"
 
-from .leafmap import *
+import os
+
+
+def in_colab_shell():
+    """Tests if the code is being executed within Google Colab."""
+    import sys
+
+    if "google.colab" in sys.modules:
+        return True
+    else:
+        return False
+
+
+def use_folium():
+    """Whether to use the folium or ipyleaflet plotting backend."""
+    if os.environ.get("USE_FOLIUM") is not None:
+        return True
+    else:
+        return False
+
+
+if in_colab_shell() or use_folium():
+    from .foliumap import *
+else:
+    from .leafmap import *
