22a758476f5c33d86b219479765c6cd485e2078f
Qiusheng Wu <giswqs@gmail.com>
2021-10-22 00:29:28 -0400

    Improved streamlit support

--- leafmap/common.py
+++ leafmap/common.py
@@ -2701,3 +2701,167 @@ def get_wms_layers(url):
     layers = list(wms.contents)
     layers.sort()
     return layers
+
+
+def create_legend(
+    legend_title="Legend",
+    legend_dict=None,
+    legend_keys=None,
+    legend_colors=None,
+    builtin_legend=None,
+    **kwargs,
+):
+    """Create a custom legend.
+
+    Args:
+        legend_title (str, optional): Title of the legend. Defaults to 'Legend'.
+        legend_dict (dict, optional): A dictionary containing legend items as keys and color as values. If provided, legend_keys and legend_colors will be ignored. Defaults to None.
+        legend_keys (list, optional): A list of legend keys. Defaults to None.
+        legend_colors (list, optional): A list of legend colors. Defaults to None.
+        builtin_legend (str, optional): Name of the builtin legend to add to the map. Defaults to None.
+
+    """
+    import pkg_resources
+    from .legends import builtin_legends
+
+    pkg_dir = os.path.dirname(pkg_resources.resource_filename("leafmap", "leafmap.py"))
+    legend_template = os.path.join(pkg_dir, "data/template/legend.html")
+
+    if "min_width" not in kwargs.keys():
+        min_width = None
+    if "max_width" not in kwargs.keys():
+        max_width = None
+    else:
+        max_width = kwargs["max_width"]
+    if "min_height" not in kwargs.keys():
+        min_height = None
+    else:
+        min_height = kwargs["min_height"]
+    if "max_height" not in kwargs.keys():
+        max_height = None
+    else:
+        max_height = kwargs["max_height"]
+    if "height" not in kwargs.keys():
+        height = None
+    else:
+        height = kwargs["height"]
+    if "width" not in kwargs.keys():
+        width = None
+    else:
+        width = kwargs["width"]
+
+    if width is None:
+        max_width = "300px"
+    if height is None:
+        max_height = "400px"
+
+    if not os.path.exists(legend_template):
+        print("The legend template does not exist.")
+        return
+
+    if legend_keys is not None:
+        if not isinstance(legend_keys, list):
+            print("The legend keys must be a list.")
+            return
+    else:
+        legend_keys = ["One", "Two", "Three", "Four", "etc"]
+
+    if legend_colors is not None:
+        if not isinstance(legend_colors, list):
+            print("The legend colors must be a list.")
+            return
+        elif all(isinstance(item, tuple) for item in legend_colors):
+            try:
+                legend_colors = [rgb_to_hex(x) for x in legend_colors]
+            except Exception as e:
+                print(e)
+        elif all((item.startswith("#") and len(item) == 7) for item in legend_colors):
+            pass
+        elif all((len(item) == 6) for item in legend_colors):
+            pass
+        else:
+            print("The legend colors must be a list of tuples.")
+            return
+    else:
+        legend_colors = [
+            "#8DD3C7",
+            "#FFFFB3",
+            "#BEBADA",
+            "#FB8072",
+            "#80B1D3",
+        ]
+
+    if len(legend_keys) != len(legend_colors):
+        print("The legend keys and values must be the same length.")
+        return
+
+    allowed_builtin_legends = builtin_legends.keys()
+    if builtin_legend is not None:
+        if builtin_legend not in allowed_builtin_legends:
+            print(
+                "The builtin legend must be one of the following: {}".format(
+                    ", ".join(allowed_builtin_legends)
+                )
+            )
+            return
+        else:
+            legend_dict = builtin_legends[builtin_legend]
+            legend_keys = list(legend_dict.keys())
+            legend_colors = list(legend_dict.values())
+
+    if legend_dict is not None:
+        if not isinstance(legend_dict, dict):
+            print("The legend dict must be a dictionary.")
+            return
+        else:
+            legend_keys = list(legend_dict.keys())
+            legend_colors = list(legend_dict.values())
+            if all(isinstance(item, tuple) for item in legend_colors):
+                try:
+                    legend_colors = [rgb_to_hex(x) for x in legend_colors]
+                except Exception as e:
+                    print(e)
+
+    header = []
+    content = []
+    footer = []
+
+    with open(legend_template) as f:
+        lines = f.readlines()
+        lines[3] = lines[3].replace("Legend", legend_title)
+        header = lines[:6]
+        footer = lines[11:]
+
+    for index, key in enumerate(legend_keys):
+        color = legend_colors[index]
+        if not color.startswith("#"):
+            color = "#" + color
+        item = "      <li><span style='background:{};'></span>{}</li>\n".format(
+            color, key
+        )
+        content.append(item)
+
+    legend_html = header + content + footer
+    legend_text = "".join(legend_html)
+    return legend_text
+
+
+def streamlit_legend(html, width=None, height=None, scrolling=True):
+    """Streamlit function to display a legend.
+
+    Args:
+        html (str): The HTML string of the legend.
+        width (str, optional): The width of the legend. Defaults to None.
+        height (str, optional): The height of the legend. Defaults to None.
+        scrolling (bool, optional): Whether to allow scrolling in the legend. Defaults to True.
+
+    """
+
+    try:
+        import streamlit.components.v1 as components
+
+        components.html(html, width=width, height=height, scrolling=scrolling)
+
+    except ImportError:
+        print("Streamlit is not installed. Please run 'pip install streamlit'.")
+        return
