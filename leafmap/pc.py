aeb8340bbecbccaaa25c42bc67bd27e792f3fe90
Qiusheng Wu <giswqs@gmail.com>
2022-01-11 00:21:05 -0500

    Added raster GUI #164
    Former-commit-id: 9ed90f6a8ad6f59c28e3a776cc625319be146447

--- leafmap/pc.py
+++ leafmap/pc.py
@@ -1,7 +1,8 @@
 """A module for Microsoft Planetary Computer (PC).
 
 """
-
+import json
+import os
 from pystac_client import Client
 from .common import stac_bands
 
@@ -58,3 +59,64 @@ def get_bands(collection, item=None):
     if item is None:
         item = get_first_item(collection, return_id=True)
     return stac_bands(collection=collection, items=item)
+
+
+def get_pc_inventory(refresh=False, verbose=False):
+    """Get the inventory of the Microsoft Planetary Computer catalog.
+
+    Args:
+        refresh (bool, optional): If True, refresh the inventory.
+        verbose (bool, optional): If True, print the collections to the console.
+
+    Returns:
+        dict: A dictionary of collections and their bands.
+    """
+    import pkg_resources
+
+    pkg_dir = os.path.dirname(pkg_resources.resource_filename("leafmap", "leafmap.py"))
+    filepath = os.path.join(pkg_dir, "data/pc_inventory.json")
+
+    if refresh:
+        catalog = Client.open(PC_ENDPOINT)
+        collections = catalog.get_children()
+        data = {}
+        for collection in collections:
+            try:
+                if verbose:
+                    print(f"{collection.id} - {collection.title}")
+                first_item = get_first_item(collection.id, return_id=True)
+                bands = stac_bands(collection=collection.id, items=first_item)
+                if isinstance(bands, list):
+                    data[collection.id] = {}
+                    data[collection.id]["title"] = collection.title
+                    data[collection.id]["first_item"] = first_item
+                    data[collection.id]["bands"] = bands
+            except Exception as e:
+                if verbose:
+                    print(f"{collection.id} has no bands.")
+
+        with open(filepath, "w") as f:
+            json.dump(data, f, indent=4)
+
+    else:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+
+    return data
+
+
+def get_collection_list():
+    """Get a list of collections in the Microsoft Planetary Computer catalog.
+
+    Returns:
+        list: A list of collections.
+    """
+    inventory = get_pc_inventory()
+
+    names = []
+
+    for key in inventory:
+        names.append(f"{key} - {inventory[key]['title']}")
+    names.sort()
+
+    return names
