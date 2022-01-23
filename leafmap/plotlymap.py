ab8a94dde5845da27018f935fc792cb2a18fee95
Qiusheng Wu <giswqs@gmail.com>
2022-01-23 00:06:56 -0500

    Added attribute table GUI #179
    Former-commit-id: e3d7f02cca9e8fa58890682afb1a0e6c4ecee754

--- leafmap/plotlymap.py
+++ leafmap/plotlymap.py
@@ -106,7 +106,7 @@ class Map(go.FigureWidget):
             refresh (bool, optional): Whether to refresh the map when the map is resized. Defaults to False.
 
         Returns:
-            Canvas: [description]
+            Canvas: Map canvas.
         """
         if not toolbar:
             super().show(**kwargs)
@@ -627,22 +627,22 @@ class Map(go.FigureWidget):
 
         fig = go.Choroplethmapbox(
             geojson=geojson_url,
-            featureidkey='properties.{}'.format(label_col),
+            featureidkey="properties.{}".format(label_col),
             locations=gdf[label_col],
             z=gdf[color_col],
             autocolorscale=False,
             colorscale=color_continuous_scale,
-            marker_line_color='peachpuff',
+            marker_line_color="peachpuff",
             colorbar=dict(
-                title={'text': "Legend"},
+                title={"text": "Legend"},
                 thickness=15,
                 len=0.35,
-                bgcolor='rgba(255,255,255,0.6)',
-                xanchor='left',
+                bgcolor="rgba(255,255,255,0.6)",
+                xanchor="left",
                 x=0.02,
-                yanchor='bottom',
-                y=0.05
-            )
+                yanchor="bottom",
+                y=0.05,
+            ),
         )
         self.add_trace(fig)
 
@@ -706,12 +706,12 @@ class Map(go.FigureWidget):
         self.set_center(center_lat, center_lon, zoom)
         del fig
 
-    def add_geojson_layer(self, geojson_in, name, color='blue', opacity=1):
+    def add_geojson_layer(self, geojson_in, name, color="blue", opacity=1):
         """Prepare proper and give style for different type of Geometry
 
         Args:
             in_geojson (str | dict): The file path or http URL to the input GeoJSON or a dictionary containing the geojson.
-            name (str): Name for the Layer 
+            name (str): Name for the Layer
             color (str, optional): Plain name for color (e.g: blue) or color code (e.g: #FF0000)
             opacity(float, optional): opacity of the layer in Map
         """
@@ -723,24 +723,24 @@ class Map(go.FigureWidget):
             data = geojson_in
         elif geojson_in.startswith("http"):
             data = requests.get(geojson_in).json()
-        elif geojson_in.lower().endswith(('.json', '.geojson')):
+        elif geojson_in.lower().endswith((".json", ".geojson")):
             with open(geojson_in) as fp:
                 data = json.load(fp)
         else:
             data = geojson_in
 
         """ Only Checking Geometry of first feature( todo : handle multiple type of Geometry in same geojson ) """
-        first_feature = data['features'][0]
-        geometry_type = first_feature['geometry']['type']
+        first_feature = data["features"][0]
+        geometry_type = first_feature["geometry"]["type"]
 
-        if(geometry_type.lower() in ["polygon", 'multipolygon']):
-            type = 'fill'
-        elif(geometry_type.lower() in ["linstring", 'multilinestring']):
-            type = 'line'
-        elif(geometry_type.lower() in ["point", "multipoint"]):
-            type = 'circle'
+        if geometry_type.lower() in ["polygon", "multipolygon"]:
+            type = "fill"
+        elif geometry_type.lower() in ["linstring", "multilinestring"]:
+            type = "line"
+        elif geometry_type.lower() in ["point", "multipoint"]:
+            type = "circle"
         else:
-            type = 'fill'
+            type = "fill"
 
         self.add_geojson(data, name, type, color, opacity)
 
@@ -749,31 +749,25 @@ class Map(go.FigureWidget):
 
         Args:
             data (dict): Geojson in Dict form
-            name (str): Name for the Layer 
+            name (str): Name for the Layer
             color (str, optional): Plain name for color (e.g: blue) or color code (e.g: #FF0000)
             opacity(float, optional): opacity of the layer in Map
         """
 
         new_layer = {
-            'source': data,
-            'name': name,
-            'type': type,
-            'opacity': opacity,
-            'color': color,
-
+            "source": data,
+            "name": name,
+            "type": type,
+            "opacity": opacity,
+            "color": color,
         }
-        if(type == 'circle'):
-            new_layer['circle'] = {
-                'radius': 5
-            }
+        if type == "circle":
+            new_layer["circle"] = {"radius": 5}
         existing_layers = list(self.layout.mapbox.layers)
 
         existing_layers.append(new_layer)
 
-        self.update_layout(
-            mapbox={
-                'layers': tuple(existing_layers)
-            })
+        self.update_layout(mapbox={"layers": tuple(existing_layers)})
 
 
 def fix_widget_error():
@@ -784,8 +778,7 @@ def fix_widget_error():
     import shutil
 
     basedatatypesPath = os.path.join(
-        os.path.dirname(
-            os.__file__), "site-packages", "plotly", "basedatatypes.py"
+        os.path.dirname(os.__file__), "site-packages", "plotly", "basedatatypes.py"
     )
 
     backup_file = basedatatypesPath.replace(".py", "_bk.py")
