595a34cf7cd64840b65c16d7ea2983f1b4ba2831
Qiusheng Wu <giswqs@gmail.com>
2022-02-15 22:52:36 -0500

    Improved docs
    Former-commit-id: 3eb83aa759452fd7e8f6de43edfced5eda3fcefa

--- leafmap/foliumap.py
+++ leafmap/foliumap.py
@@ -321,6 +321,12 @@ class Map(folium.Map):
     ):
         """Add a local raster dataset to the map.
 
+            If you are using this function in JupyterHub on a remote server (e.g., Binder, Microsoft Planetary Computer),
+            try adding to following two lines to the beginning of the notebook if the raster does not render properly.
+
+            import os
+            os.environ['LOCALTILESERVER_CLIENT_PREFIX'] = f'{os.environ['JUPYTERHUB_SERVICE_PREFIX'].lstrip('/')}/proxy/{{port}}'
+
         Args:
             source (str): The path to the GeoTIFF file or the URL of the Cloud Optimized GeoTIFF.
             band (int, optional): The band to use. Band indexing starts at 1. Defaults to None.
