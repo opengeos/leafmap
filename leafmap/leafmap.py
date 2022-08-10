75c22096a8e1e1cbed232d10b58bdd935d9b6734
Qiusheng Wu <giswqs@gmail.com>
2022-08-10 09:44:44 -0400

    Updated Map.to_streamlit()
    Former-commit-id: 16a948aa52d7f1c1b02448d5bfd9c6c7019fe0af

--- leafmap/leafmap.py
+++ leafmap/leafmap.py
@@ -1579,13 +1579,11 @@ class Map(ipyleaflet.Map):
         screenshot = screen_capture(outfile, monitor)
         self.screenshot = screenshot
 
-    def to_streamlit(
-        self, width=700, height=600, responsive=True, scrolling=False, **kwargs
-    ):
+    def to_streamlit(self, width=None, height=600, scrolling=False, **kwargs):
         """Renders map figure in a Streamlit app.
 
         Args:
-            width (int, optional): Width of the map. Defaults to 700.
+            width (int, optional): Width of the map. Defaults to None.
             height (int, optional): Height of the map. Defaults to 600.
             responsive (bool, optional): Whether to make the map responsive. Defaults to True.
             scrolling (bool, optional): If True, show a scrollbar when the content is larger than the iframe. Otherwise, do not show a scrollbar. Defaults to False.
@@ -1595,16 +1593,15 @@ class Map(ipyleaflet.Map):
         """
 
         try:
-            import streamlit as st
             import streamlit.components.v1 as components
 
-            if responsive:
-                make_map_responsive = """
-                <style>
-                [title~="st.iframe"] { width: 100%}
-                </style>
-                """
-                st.markdown(make_map_responsive, unsafe_allow_html=True)
+            # if responsive:
+            #     make_map_responsive = """
+            #     <style>
+            #     [title~="st.iframe"] { width: 100%}
+            #     </style>
+            #     """
+            #     st.markdown(make_map_responsive, unsafe_allow_html=True)
             return components.html(
                 self.to_html(), width=width, height=height, scrolling=scrolling
             )
