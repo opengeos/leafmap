23368265ef9cf4267ce7f3cb0d82162ba39ecf92
Qiusheng Wu <giswqs@gmail.com>
2022-09-07 22:30:43 -0400

    Bump version: 0.10.4 → 0.10.5
    Former-commit-id: dc7a40c67404c6e68b4b7dc7d72102c74ae41a33

--- leafmap/__init__.py
+++ leafmap/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = "giswqs@gmail.com"
-__version__ = "0.10.4"
+__version__ = "0.10.5"
 
 import os
 
--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/leafmap",
-    version="0.10.4",
+    version="0.10.5",
     zip_safe=False,
 )
