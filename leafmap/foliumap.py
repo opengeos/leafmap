958ad1710841cbd981afbb8e71ca55a8f911831f
Qiusheng Wu <giswqs@gmail.com>
2022-10-30 21:55:15 -0400

    Improved split control
    Former-commit-id: 5350a819c366ae570ce3e80132a25d6ed9d6119e

--- leafmap/foliumap.py
+++ leafmap/foliumap.py
@@ -2168,6 +2168,10 @@ class Map(folium.Map):
             left_layer (str, optional): The layer tile layer. Defaults to 'TERRAIN'.
             right_layer (str, optional): The right tile layer. Defaults to 'OpenTopoMap'.
         """
+        if "max_zoom" not in kwargs:
+            kwargs["max_zoom"] = 100
+        if "max_native_zoom" not in kwargs:
+            kwargs["max_native_zoom"] = 100
         try:
             if left_layer in basemaps.keys():
                 left_layer = basemaps[left_layer]
@@ -2179,6 +2183,7 @@ class Map(folium.Map):
                         name="Left Layer",
                         attr=" ",
                         overlay=True,
+                        **kwargs,
                     )
                 else:
                     left_layer = folium.raster_layers.TileLayer(
@@ -2186,6 +2191,7 @@ class Map(folium.Map):
                         name="Left Layer",
                         attr=" ",
                         overlay=True,
+                        **kwargs,
                     )
             elif isinstance(left_layer, folium.raster_layers.TileLayer) or isinstance(
                 left_layer, folium.WmsTileLayer
@@ -2206,6 +2212,7 @@ class Map(folium.Map):
                         name="Right Layer",
                         attr=" ",
                         overlay=True,
+                        **kwargs,
                     )
                 else:
                     right_layer = folium.raster_layers.TileLayer(
@@ -2213,6 +2220,7 @@ class Map(folium.Map):
                         name="Right Layer",
                         attr=" ",
                         overlay=True,
+                        **kwargs,
                     )
             elif isinstance(right_layer, folium.raster_layers.TileLayer) or isinstance(
                 left_layer, folium.WmsTileLayer
