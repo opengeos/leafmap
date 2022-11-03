7dbb0caf20845bc7a88b3f2dea36cf81d612d050
Qiusheng Wu <giswqs@gmail.com>
2022-11-03 14:38:52 -0400

    Bump version: 0.11.2 → 0.11.3
    Former-commit-id: d44908d299051b69e2436d40fa5912a46b86ad97

--- setup.py
+++ setup.py
@@ -73,6 +73,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/leafmap",
-    version="0.11.2",
+    version="0.11.3",
     zip_safe=False,
 )
