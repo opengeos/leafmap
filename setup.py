e0b3d27f3292ab1a4f86141de0c85e31bbc48509
Qiusheng Wu <giswqs@gmail.com>
2023-03-26 09:18:06 -0400

    Bump version: 0.18.7 → 0.18.8

--- setup.py
+++ setup.py
@@ -81,6 +81,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/leafmap",
-    version="0.18.7",
+    version="0.18.8",
     zip_safe=False,
 )
