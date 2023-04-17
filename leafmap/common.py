9c0b3a306ff4f35902d87414d3f9f6fb98820343
Qiusheng Wu <giswqs@gmail.com>
2023-04-17 18:58:16 -0400

    Added tms_to_geotiff function (#420)
    * Add tms_to_geotiff function
    
    * Fixed typos
    
    * Skip notebook 72
    
    * Fixed notebook error

--- leafmap/common.py
+++ leafmap/common.py
@@ -8602,3 +8602,286 @@ def s3_download_files(
         if not quiet:
             print(f"Downloading {index+1} of {len(keys)}: {outfile}")
         s3_download_file(bucket=bucket, key=key, outfile=outfile, **kwargs)
+
+
+def tms_to_geotiff(
+    output,
+    bbox,
+    zoom=None,
+    resolution=None,
+    source="OpenStreetMap",
+    to_cog=False,
+    quiet=False,
+    **kwargs,
+):
+    """Download TMS tiles and convert them to a GeoTIFF. The source is adapted from https://github.com/gumblex/tms2geotiff.
+        Credits to the GitHub user @gumblex.
+
+    Args:
+        output (str): The output GeoTIFF file.
+        bbox (list): The bounding box [minx, miny, maxx, maxy], e.g., [-122.5216, 37.733, -122.3661, 37.8095]
+        zoom (int, optional): The map zoom level. Defaults to None.
+        resolution (float, optional): The resolution in meters. Defaults to None.
+        source (str, optional): The tile source. It can be one of the following: "OPENSTREETMAP", "ROADMAP",
+            "SATELLITE", "TERRAIN", "HYBRID", or an HTTP URL. Defaults to "OpenStreetMap".
+        to_cog (bool, optional): Convert to Cloud Optimized GeoTIFF. Defaults to False.
+        quiet (bool, optional): Suppress output. Defaults to False.
+        **kwargs: Additional arguments to pass to gdal.GetDriverByName("GTiff").Create().
+
+    """
+
+    import io
+    import math
+    import itertools
+    import concurrent.futures
+
+    import numpy
+    from PIL import Image
+
+    try:
+        from osgeo import gdal, osr
+    except ImportError:
+        raise ImportError("GDAL is not installed. Install it with pip install GDAL")
+
+    try:
+        import httpx
+
+        SESSION = httpx.Client()
+    except ImportError:
+        import requests
+
+        SESSION = requests.Session()
+
+    xyz_tiles = {
+        "OPENSTREETMAP": {
+            "url": "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+            "attribution": "OpenStreetMap",
+            "name": "OpenStreetMap",
+        },
+        "ROADMAP": {
+            "url": "https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
+            "attribution": "Google",
+            "name": "Google Maps",
+        },
+        "SATELLITE": {
+            "url": "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+            "attribution": "Google",
+            "name": "Google Satellite",
+        },
+        "TERRAIN": {
+            "url": "https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}",
+            "attribution": "Google",
+            "name": "Google Terrain",
+        },
+        "HYBRID": {
+            "url": "https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+            "attribution": "Google",
+            "name": "Google Satellite",
+        },
+    }
+
+    if isinstance(source, str) and source.upper() in xyz_tiles:
+        source = xyz_tiles[source.upper()]["url"]
+    elif isinstance(source, str) and source.startswith("http"):
+        pass
+    else:
+        raise ValueError(
+            'source must be one of "OpenStreetMap", "ROADMAP", "SATELLITE", "TERRAIN", "HYBRID", or a URL'
+        )
+
+    def resolution_to_zoom_level(resolution):
+        """
+        Convert map resolution in meters to zoom level for Web Mercator (EPSG:3857) tiles.
+        """
+        # Web Mercator tile size in meters at zoom level 0
+        initial_resolution = 156543.03392804097
+
+        # Calculate the zoom level
+        zoom_level = math.log2(initial_resolution / resolution)
+
+        return int(zoom_level)
+
+    if isinstance(bbox, list) and len(bbox) == 4:
+        west, south, east, north = bbox
+    else:
+        raise ValueError(
+            "bbox must be a list of 4 coordinates in the format of [xmin, ymin, xmax, ymax]"
+        )
+
+    if zoom is None and resolution is None:
+        raise ValueError("Either zoom or resolution must be provided")
+    elif zoom is not None and resolution is not None:
+        raise ValueError("Only one of zoom or resolution can be provided")
+
+    if resolution is not None:
+        zoom = resolution_to_zoom_level(resolution)
+
+    EARTH_EQUATORIAL_RADIUS = 6378137.0
+
+    Image.MAX_IMAGE_PIXELS = None
+
+    gdal.UseExceptions()
+    web_mercator = osr.SpatialReference()
+    web_mercator.ImportFromEPSG(3857)
+
+    WKT_3857 = web_mercator.ExportToWkt()
+
+    def from4326_to3857(lat, lon):
+        xtile = math.radians(lon) * EARTH_EQUATORIAL_RADIUS
+        ytile = (
+            math.log(math.tan(math.radians(45 + lat / 2.0))) * EARTH_EQUATORIAL_RADIUS
+        )
+        return (xtile, ytile)
+
+    def deg2num(lat, lon, zoom):
+        lat_r = math.radians(lat)
+        n = 2**zoom
+        xtile = (lon + 180) / 360 * n
+        ytile = (1 - math.log(math.tan(lat_r) + 1 / math.cos(lat_r)) / math.pi) / 2 * n
+        return (xtile, ytile)
+
+    def is_empty(im):
+        extrema = im.getextrema()
+        if len(extrema) >= 3:
+            if len(extrema) > 3 and extrema[-1] == (0, 0):
+                return True
+            for ext in extrema[:3]:
+                if ext != (0, 0):
+                    return False
+            return True
+        else:
+            return extrema[0] == (0, 0)
+
+    def paste_tile(bigim, base_size, tile, corner_xy, bbox):
+        if tile is None:
+            return bigim
+        im = Image.open(io.BytesIO(tile))
+        mode = "RGB" if im.mode == "RGB" else "RGBA"
+        size = im.size
+        if bigim is None:
+            base_size[0] = size[0]
+            base_size[1] = size[1]
+            newim = Image.new(
+                mode, (size[0] * (bbox[2] - bbox[0]), size[1] * (bbox[3] - bbox[1]))
+            )
+        else:
+            newim = bigim
+
+        dx = abs(corner_xy[0] - bbox[0])
+        dy = abs(corner_xy[1] - bbox[1])
+        xy0 = (size[0] * dx, size[1] * dy)
+        if mode == "RGB":
+            newim.paste(im, xy0)
+        else:
+            if im.mode != mode:
+                im = im.convert(mode)
+            if not is_empty(im):
+                newim.paste(im, xy0)
+        im.close()
+        return newim
+
+    def finish_picture(bigim, base_size, bbox, x0, y0, x1, y1):
+        xfrac = x0 - bbox[0]
+        yfrac = y0 - bbox[1]
+        x2 = round(base_size[0] * xfrac)
+        y2 = round(base_size[1] * yfrac)
+        imgw = round(base_size[0] * (x1 - x0))
+        imgh = round(base_size[1] * (y1 - y0))
+        retim = bigim.crop((x2, y2, x2 + imgw, y2 + imgh))
+        if retim.mode == "RGBA" and retim.getextrema()[3] == (255, 255):
+            retim = retim.convert("RGB")
+        bigim.close()
+        return retim
+
+    def get_tile(url):
+        retry = 3
+        while 1:
+            try:
+                r = SESSION.get(url, timeout=60)
+                break
+            except Exception:
+                retry -= 1
+                if not retry:
+                    raise
+        if r.status_code == 404:
+            return None
+        elif not r.content:
+            return None
+        r.raise_for_status()
+        return r.content
+
+    def draw_tile(
+        source, lat0, lon0, lat1, lon1, zoom, filename, quiet=False, **kwargs
+    ):
+        x0, y0 = deg2num(lat0, lon0, zoom)
+        x1, y1 = deg2num(lat1, lon1, zoom)
+        if x0 > x1:
+            x0, x1 = x1, x0
+        if y0 > y1:
+            y0, y1 = y1, y0
+        corners = tuple(
+            itertools.product(
+                range(math.floor(x0), math.ceil(x1)),
+                range(math.floor(y0), math.ceil(y1)),
+            )
+        )
+        totalnum = len(corners)
+        futures = []
+        with concurrent.futures.ThreadPoolExecutor(5) as executor:
+            for x, y in corners:
+                futures.append(
+                    executor.submit(get_tile, source.format(z=zoom, x=x, y=y))
+                )
+            bbox = (math.floor(x0), math.floor(y0), math.ceil(x1), math.ceil(y1))
+            bigim = None
+            base_size = [256, 256]
+            for k, (fut, corner_xy) in enumerate(zip(futures, corners), 1):
+                bigim = paste_tile(bigim, base_size, fut.result(), corner_xy, bbox)
+                if not quiet:
+                    print("Downloaded image %d/%d" % (k, totalnum))
+
+        if not quiet:
+            print("Saving GeoTIFF. Please wait...")
+        img = finish_picture(bigim, base_size, bbox, x0, y0, x1, y1)
+        imgbands = len(img.getbands())
+        driver = gdal.GetDriverByName("GTiff")
+
+        if "options" not in kwargs:
+            kwargs["options"] = [
+                "COMPRESS=DEFLATE",
+                "PREDICTOR=2",
+                "ZLEVEL=9",
+                "TILED=YES",
+            ]
+
+        gtiff = driver.Create(
+            filename,
+            img.size[0],
+            img.size[1],
+            imgbands,
+            gdal.GDT_Byte,
+            **kwargs,
+        )
+        xp0, yp0 = from4326_to3857(lat0, lon0)
+        xp1, yp1 = from4326_to3857(lat1, lon1)
+        pwidth = abs(xp1 - xp0) / img.size[0]
+        pheight = abs(yp1 - yp0) / img.size[1]
+        gtiff.SetGeoTransform((min(xp0, xp1), pwidth, 0, max(yp0, yp1), 0, -pheight))
+        gtiff.SetProjection(WKT_3857)
+        for band in range(imgbands):
+            array = numpy.array(img.getdata(band), dtype="u8")
+            array = array.reshape((img.size[1], img.size[0]))
+            band = gtiff.GetRasterBand(band + 1)
+            band.WriteArray(array)
+        gtiff.FlushCache()
+
+        if not quiet:
+            print(f"Image saved to {filename}")
+        return img
+
+    try:
+        draw_tile(source, south, west, north, east, zoom, output, quiet, **kwargs)
+        if to_cog:
+            image_to_cog(output, output)
+    except Exception as e:
+        raise Exception(e)
