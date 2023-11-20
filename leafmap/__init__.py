68cbfe2e079c3ba04eb4dc50ff7faa7024251153
Qiusheng Wu <giswqs@gmail.com>
2023-11-20 16:50:35 -0500

    Add tooltip for PMTiles

--- leafmap/__init__.py
+++ leafmap/__init__.py
@@ -63,7 +63,7 @@ def view_vector(
             **kwargs: Additional keyword arguments that will be passed to lonboard.Layer.from_geopandas()
 
     Returns:
-        None
+        lonboard.Map: A lonboard Map object.
     """
     from .deckgl import Map
 
@@ -79,6 +79,46 @@ def view_vector(
         color_args,
         open_args,
         **kwargs,
+    )
+    return m
+
+
+def view_pmtiles(
+    url,
+    style=None,
+    name=None,
+    tooltip=True,
+    overlay=True,
+    control=True,
+    show=True,
+    zoom_to_layer=True,
+    map_args={},
+    **kwargs,
+):
+    """
+    Visualize PMTiles the map.
+
+    Args:
+        url (str): The URL of the PMTiles file.
+        style (str, optional): The CSS style to apply to the layer. Defaults to None.
+            See https://docs.mapbox.com/style-spec/reference/layers/ for more info.
+        name (str, optional): The name of the layer. Defaults to None.
+        tooltip (bool, optional): Whether to show a tooltip when hovering over the layer. Defaults to True.
+        overlay (bool, optional): Whether the layer should be added as an overlay. Defaults to True.
+        control (bool, optional): Whether to include the layer in the layer control. Defaults to True.
+        show (bool, optional): Whether the layer should be shown initially. Defaults to True.
+        zoom_to_layer (bool, optional): Whether to zoom to the layer extent. Defaults to True.
+        **kwargs: Additional keyword arguments to pass to the PMTilesLayer constructor.
+
+    Returns:
+        folium.Map: A folium Map object.
+    """
+
+    from .foliumap import Map
+
+    m = Map(**map_args)
+    m.add_pmtiles(
+        url, style, name, tooltip, overlay, control, show, zoom_to_layer, **kwargs
     )
     return m
 
