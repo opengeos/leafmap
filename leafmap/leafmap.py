541b762ac100ef8a15ee266441fef888fec0e9a0
Qiusheng Wu <giswqs@gmail.com>
2023-12-28 22:24:59 -0500

    Update color_scale in add_velocity

--- leafmap/leafmap.py
+++ leafmap/leafmap.py
@@ -3861,23 +3861,7 @@ class Map(ipyleaflet.Map):
         max_velocity=20,
         display_options={},
         name="Velocity",
-        color_scale=[
-            "rgb(36,104, 180)",
-            "rgb(60,157, 194)",
-            "rgb(128,205,193)",
-            "rgb(151,218,168)",
-            "rgb(198,231,181)",
-            "rgb(238,247,217)",
-            "rgb(255,238,159)",
-            "rgb(252,217,125)",
-            "rgb(255,182,100)",
-            "rgb(252,150,75)",
-            "rgb(250,112,52)",
-            "rgb(245,64,32)",
-            "rgb(237,45,28)",
-            "rgb(220,24,32)",
-            "rgb(180,0,35)"
-        ]
+        color_scale=None
     ):
         """Add a velocity layer to the map.
 
@@ -3929,6 +3913,25 @@ class Map(ipyleaflet.Map):
         if level_dimension in coords:
             ds = ds.isel(drop=True, **params)
 
+        if color_scale is None:
+            color_scale=[
+                "rgb(36,104, 180)",
+                "rgb(60,157, 194)",
+                "rgb(128,205,193)",
+                "rgb(151,218,168)",
+                "rgb(198,231,181)",
+                "rgb(238,247,217)",
+                "rgb(255,238,159)",
+                "rgb(252,217,125)",
+                "rgb(255,182,100)",
+                "rgb(252,150,75)",
+                "rgb(250,112,52)",
+                "rgb(245,64,32)",
+                "rgb(237,45,28)",
+                "rgb(220,24,32)",
+                "rgb(180,0,35)"
+            ]
+        
         wind = Velocity(
             data=ds,
             zonal_speed=zonal_speed,
