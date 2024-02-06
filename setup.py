6708fdc22e1e3477c396c58dcbacff3e1299a235
Qiusheng Wu <giswqs@gmail.com>
2024-02-06 11:51:10 -0500

    Bump version: 0.30.1 → 0.31.0

--- setup.py
+++ setup.py
@@ -98,6 +98,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/opengeos/leafmap",
-    version="0.30.1",
+    version="0.31.0",
     zip_safe=False,
 )
