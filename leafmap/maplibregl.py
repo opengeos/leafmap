f92dbe5e560d5a4093ebf03a029684a1f06ec2f4
Qiusheng Wu <giswqs@gmail.com>
2024-04-14 21:51:11 -0400

    Add add_basemap and set_center methods

--- leafmap/maplibregl.py
+++ leafmap/maplibregl.py
@@ -1,8 +1,16 @@
 """The maplibregl module provides the Map class for creating interactive maps using the maplibre.ipywidget module.
 """
 
+import xyzservices
+from box import Box
 from maplibre.ipywidget import MapWidget
 from maplibre import Layer, LayerType, MapOptions
+from maplibre.sources import GeoJSONSource, RasterTileSource
+
+from .basemaps import xyz_to_leaflet
+from .common import *
+
+basemaps = Box(xyz_to_leaflet(), frozen_box=True)
 
 
 class Map(MapWidget):
@@ -22,3 +30,121 @@ class Map(MapWidget):
         map_options = MapOptions(center=center, zoom=zoom, **kwargs)
 
         super().__init__(map_options, height=height)
+
+    def set_center(self, lon, lat, zoom=None):
+        """Sets the center of the map.
+
+        This method sets the center of the map to the specified longitude and latitude.
+        If a zoom level is provided, it also sets the zoom level of the map.
+
+        Args:
+            lon (float): The longitude of the center of the map.
+            lat (float): The latitude of the center of the map.
+            zoom (int, optional): The zoom level of the map. If None, the zoom level is not changed.
+
+        Returns:
+            None
+        """
+        center = [lon, lat]
+        self.add_call("setCenter", center)
+
+        if zoom is not None:
+            self.add_call("setZoom", zoom)
+
+    def set_zoom(self, zoom):
+        """
+        Sets the zoom level of the map.
+
+        This method sets the zoom level of the map to the specified value.
+
+        Args:
+            zoom (int): The zoom level of the map.
+
+        Returns:
+            None
+        """
+        self.add_call("setZoom", zoom)
+
+    def fit_bounds(self, bounds):
+        """
+        Adjusts the viewport of the map to fit the specified geographical bounds in the format of [[lon_min, lat_min], [lon_max, lat_max]].
+
+        This method adjusts the viewport of the map so that the specified geographical bounds
+        are visible in the viewport. The bounds are specified as a list of two points,
+        where each point is a list of two numbers representing the longitude and latitude.
+
+        Args:
+            bounds (list): A list of two points representing the geographical bounds that
+                        should be visible in the viewport. Each point is a list of two
+                        numbers representing the longitude and latitude. For example,
+                        [[32.958984, -5.353521],[43.50585, 5.615985]]
+
+        Returns:
+            None
+        """
+
+        self.add_call("fitBounds", bounds)
+
+    def add_basemap(self, basemap="HYBRID", show=True, **kwargs):
+        """Adds a basemap to the map.
+
+        Args:
+            basemap (str, optional): Can be one of string from basemaps. Defaults to 'HYBRID'.
+            show (bool, optional): Whether the basemap is visible or not. Defaults to True.
+            **kwargs: Additional keyword arguments that are passed to the RasterTileSource class.
+                See https://eodagmbh.github.io/py-maplibregl/api/sources/#maplibre.sources.RasterTileSource for more information.
+        """
+
+        map_dict = {
+            "ROADMAP": "Google Maps",
+            "SATELLITE": "Google Satellite",
+            "TERRAIN": "Google Terrain",
+            "HYBRID": "Google Hybrid",
+        }
+
+        name = basemap
+        url = None
+        attribution = None
+        max_zoom = 30
+        min_zoom = 0
+
+        if isinstance(basemap, str) and basemap.upper() in map_dict:
+            layer = get_google_map(basemap.upper(), **kwargs)
+            url = layer.url
+            name = layer.name
+            attribution = layer.attribution
+
+        elif isinstance(basemap, xyzservices.TileProvider):
+            name = basemap.name
+            url = basemap.build_url()
+            attribution = basemap.attribution
+            if "max_zoom" in basemap.keys():
+                max_zoom = basemap["max_zoom"]
+            if "min_zoom" in basemap.keys():
+                min_zoom = basemap["min_zoom"]
+
+        elif basemap in basemaps:
+            url = basemaps[basemap]["url"]
+            attribution = basemaps[basemap]["attribution"]
+            if "max_zoom" in basemaps[basemap]:
+                max_zoom = basemaps[basemap]["max_zoom"]
+            if "min_zoom" in basemaps[basemap]:
+                min_zoom = basemaps[basemap]["min_zoom"]
+        else:
+            print(
+                "Basemap can only be one of the following:\n  {}".format(
+                    "\n  ".join(basemaps.keys())
+                )
+            )
+            return
+
+        raster_source = RasterTileSource(
+            tiles=[url],
+            attribution=attribution,
+            max_zoom=max_zoom,
+            min_zoom=min_zoom,
+            **kwargs,
+        )
+        layer = Layer(id=name, source=raster_source, type=LayerType.RASTER)
+        self.add_layer(layer)
+        self.set_visibility(name, show)
