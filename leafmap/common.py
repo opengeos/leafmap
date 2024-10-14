da1d5792fdf51d342ee669ef3436e88ee8808db9
Qiusheng Wu <giswqs@gmail.com>
2024-10-13 20:46:47 -0400

    Fix gdal and proj errors for map tiles

--- leafmap/common.py
+++ leafmap/common.py
@@ -9220,7 +9220,7 @@ def map_tiles_to_geotiff(
         **kwargs: Additional arguments to pass to gdal.GetDriverByName("GTiff").Create().
 
     """
-
+    import re
     import io
     import math
     import itertools
@@ -9325,7 +9325,18 @@ def map_tiles_to_geotiff(
 
     gdal.UseExceptions()
     web_mercator = osr.SpatialReference()
-    web_mercator.ImportFromEPSG(3857)
+    try:
+        web_mercator.ImportFromEPSG(3857)
+    except RuntimeError as e:
+        # https://github.com/PDAL/PDAL/issues/2544#issuecomment-637995923
+        if "PROJ" in str(e):
+            pattern = r"/[\w/]+"
+            match = re.search(pattern, str(e))
+            if match:
+                file_path = match.group(0)
+                os.environ["PROJ_LIB"] = file_path
+                os.environ["GDAL_DATA"] = file_path.replace("proj", "gdal")
+                web_mercator.ImportFromEPSG(3857)
 
     WKT_3857 = web_mercator.ExportToWkt()
 
