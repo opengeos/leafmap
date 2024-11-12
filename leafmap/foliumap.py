613e6cc08808fd27c4c4ca3671c79b31ccd83078
Myles Scolnick <myles@marimo.io>
2024-11-12 16:09:10 -0500

    Remove dependency on plotly in foliummap

--- leafmap/foliumap.py
+++ leafmap/foliumap.py
@@ -9,7 +9,6 @@ from .basemaps import xyz_to_folium
 from .osm import *
 from . import examples
 from .map_widgets import *
-from .plot import *
 
 from branca.element import Figure, JavascriptLink, MacroElement
 from folium.elements import JSCSSMixin
